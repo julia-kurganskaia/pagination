54509c5088e9cef064f46173fe4e3055347050ea
Julia Kurganskaia <yulia.kurganskaya@gmail.com>
2021-04-13 13:29:23 +1200

    Add active class in pagination

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const pages = [1, 2, 3, 4, 5, 6];
 
 function App() {
+  const [ currentPage, setCurrentPage ] = useState(1);
+  function handleClick(e) {
+    e.preventDefault();
+
+    setCurrentPage(Number(e.target.dataset.page));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Pagination</h1>
+        <div>
+          {pages.map((page) => {
+            // console.log(index)
+            return (
+              <a href="/" className={`Pagination${page === currentPage ? " Active" : ""}`} key={page} data-page={page} onClick={handleClick}>{page}</a>
+            )})
+          }
+        </div>
       </header>
     </div>
   );
