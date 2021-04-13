0fde56ec42f2f5e3a8f8ef3a7e283b94326fde4b
Julia Kurganskaia <yulia.kurganskaya@gmail.com>
2021-04-13 22:58:21 +1200

    Display each page with certain number of items

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-
-const pages = [1, 2, 3, 4, 5, 6];
+import { makeApage } from "./pag/pagination";
 
 function App() {
+
   const [ currentPage, setCurrentPage ] = useState(1);
+  const page = makeApage(currentPage);
+  const pagesNumber = Math.ceil(page.totalItemsNumber / page.itemsPerPage);
+
+  let pages = [];
+
+  for (let i = 1; i <= pagesNumber; i++) {
+    pages.push(i);
+  }
+
   function handleClick(e) {
     e.preventDefault();
 
     setCurrentPage(Number(e.target.dataset.page));
-  }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Pagination</h1>
+        <div className="ItemsToDisplay">
+          {page.itemsToShow.map((item) => {
+            return (
+              <div key={item} className="Item">{item}</div>
+            )
+          })}
+        </div>
         <div>
           {pages.map((page) => {
-            // console.log(index)
             return (
               <a href="/" className={`Pagination${page === currentPage ? " Active" : ""}`} key={page} data-page={page} onClick={handleClick}>{page}</a>
             )})
@@ -28,6 +43,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pag/pagination.js
@@ -0,0 +1,20 @@
+//@ts-check
+
+const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42];
+
+export function makeApage(pageNumber) {
+  let pageInfo = {
+    totalItemsNumber: 42,
+    itemsPerPage: 10,
+    itemsToShow: [],
+  };
+  const itemsIndex = pageNumber * pageInfo.itemsPerPage - pageInfo.itemsPerPage;
+  let arr = [];
+  for (let i = itemsIndex; i < items.length; i++) {
+    if (arr.length < pageInfo.itemsPerPage) {
+      arr.push(items[i]);
+    }
+  }
+  pageInfo.itemsToShow = arr;
+  return pageInfo;
+};
