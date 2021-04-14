37251df079a168bd7eb2a51b9c4e8a8539351df9
Julia Kurganskaia <yulia.kurganskaya@gmail.com>
2021-04-15 10:00:02 +1200

    Use Readline Module to read the file with data

--- server.js
+++ server.js
@@ -2,15 +2,28 @@
 
 const express = require("express");
 const app = express();
+const fs = require("fs");
+const readline = require("readline");
 
 app.set("port", process.env.API_PORT || 3001);
 
-const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42];
+let items = [];
+
+const file = readline.createInterface({
+  input: fs.createReadStream("./book-quotes.txt"),
+  crlfDelay: Infinity
+});
+
+file.on('line', (line) => {
+  items.push(line)
+});
+
+console.log(items)
 
 function makeApage(pageNumber) {
   let pageInfo = {
     totalItemsNumber: 42,
-    itemsPerPage: 10,
+    itemsPerPage: 5,
     itemsToShow: [],
   };
   const itemsIndex = (pageNumber - 1) * pageInfo.itemsPerPage;
