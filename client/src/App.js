cc5db9461ce8cf1e9a30d28edcbbe005d6f40d55
Julia Kurganskaia <yulia.kurganskaya@gmail.com>
2021-04-14 22:21:21 +1200

    Implement number server API

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
+//@ts-check
+
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
-import { makeApage } from "./pag/pagination";
+import { useState, useEffect } from "react";
 
 function App() {
   const [ currentPage, setCurrentPage ] = useState(1);
-  const page = makeApage(currentPage);
-  const pagesNumber = Math.ceil(page.totalItemsNumber / page.itemsPerPage);
+  const [ totalItemsNumber, setTotalItemsNumber ] = useState(0);
+  const [ itemsPerPage, setItemsPerPage ] = useState(1);
+  const [ itemsToShow, setItemsToShow ] = useState([]);
+
+  const pagesNumber = Math.ceil(totalItemsNumber / itemsPerPage);
+
+  useEffect(() => {
+    fetch(`/api/v1/something?page=${currentPage}`)
+      .then(response => response.json())
+      .then(data => {
+        setTotalItemsNumber(data.totalItemsNumber);
+        setItemsPerPage(data.itemsPerPage);
+        setItemsToShow(data.itemsToShow);
+      });
+  }, [currentPage]);
 
   let pages = [];
 
@@ -26,7 +40,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Pagination</h1>
         <div className="ItemsToDisplay">
-          {page.itemsToShow.map((item) => {
+          {itemsToShow.map((item) => {
             return (
               <div key={item} className="Item">{item}</div>
             )
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 //@ts-check
 
+const express = require("express");
+const app = express();
+
+app.set("port", process.env.API_PORT || 3001);
+
 const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42];
 
-export function makeApage(pageNumber) {
+function makeApage(pageNumber) {
   let pageInfo = {
     totalItemsNumber: 42,
     itemsPerPage: 10,
@@ -18,3 +23,14 @@ export function makeApage(pageNumber) {
   pageInfo.itemsToShow = arr;
   return pageInfo;
 };
+
+app.get("/api/v1/something", (req, res) => {
+  const pageNumber = Number(req.query.page);
+  const result = makeApage(pageNumber);
+
+  res.json(result);
+});
+
+app.listen(app.get("port"), () => {
+  console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
+});
