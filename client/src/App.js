b6bfc122c9e689e4d555066481cf7f2b98802963
Julia Kurganskaia <yulia.kurganskaya@gmail.com>
2021-04-23 11:08:30 +1200

    Implement throbber

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 //@ts-check
 
+import "@fortawesome/fontawesome-free/css/all.css";
 import "./App.css";
 import { useState, useEffect } from "react";
 
@@ -8,16 +9,20 @@ function App() {
   const [ totalItemsNumber, setTotalItemsNumber ] = useState(0);
   const [ itemsPerPage, setItemsPerPage ] = useState(1);
   const [ itemsToShow, setItemsToShow ] = useState([]);
+  const [ showThrobber, setShowThrobber ] = useState(false);
 
   const pagesNumber = Math.ceil(totalItemsNumber / itemsPerPage);
 
   useEffect(() => {
+    setShowThrobber(true);
+
     fetch(`/api/v1/something?page=${currentPage}`)
       .then(response => response.json())
       .then(data => {
         setTotalItemsNumber(data.totalItemsNumber);
         setItemsPerPage(data.itemsPerPage);
         setItemsToShow(data.itemsToShow);
+        setShowThrobber(false);
       });
   }, [currentPage]);
 
@@ -37,12 +42,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>The 100 Most Iconic Book Quotes</h1>
+        <div className="fa-3x">
+          {showThrobber === true ? <i className="fas fa-spinner fa-pulse"></i> : null}
+        </div>
         <div className="ItemsToDisplay">
-          {itemsToShow.map((item) => {
+          {showThrobber === false ? itemsToShow.map((item) => {
             return (
               <div key={item} className="Item">{item}</div>
             )
-          })}
+          }) : null}
         </div>
         <div className="Pages-container">
           {pages.map((page) => {
